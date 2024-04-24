97bb89a1d6dc95360700a741a31496415e1d5716
Gloeend <alexey.larionov.fl@gmail.com>
2024-04-24 16:18:04 +0700

    upd

--- src/popup/Profile/AddMail.tsx
+++ src/popup/Profile/AddMail.tsx
@@ -1,39 +1,45 @@
-import { useState } from "react";
-import { useUpdateMeMutation } from "../../api/authApi";
+import {useState} from "react";
+import {useUpdateMeMutation} from "../../api/authApi";
+import {useAppSelector} from "../../store/storeHooks.ts";
+import {selectMe} from "../../store/slices/authSlice.ts";
 
 
 type Props = {
-  close: () => void;
-  goTo: (path: string) => void;
+    close: () => void;
+    goTo: (path: string) => void;
 }
 
 
 const AddMail = (props: Props) => {
-  const { close, goTo } = props;
-  const [mail, setMail] = useState('');
-  const [updMe] = useUpdateMeMutation()
+    const {close, goTo} = props;
+    const [mail, setMail] = useState('');
+    const [updMe] = useUpdateMeMutation();
+    const me = useAppSelector(selectMe);
 
-  const click = () => {
-    updMe({ email: mail }).unwrap()
-  }
-  return (
-    <>
-      <div className="popup__row popup__row--title">
-        <div onClick={() => goTo('settings')} className="popup__btn popup__btn--back"></div>
-        <div className="popup__title">Добавить почту</div>
-        <div onClick={close} className="popup__btn popup__btn--close"></div>
-      </div>
-      <div className="popup__row popup__row--centered">
-        <div className="profile__input-simple-wrapper">
-          <div className="profile__input-simple-row">
-            <div className="profile__input-simple-title">Почта</div>
-            <input className="profile__input-simple-input" type="text" name="" value={mail} onChange={(e) => setMail(e.target.value)} placeholder="" />
-          </div>
-        </div>
-      </div>
-      <div onClick={click} className="button button--red">Применить</div>
-    </>
-  )
+    const click = () => {
+        updMe({email: mail}).unwrap().then(
+            _ => goTo("settings")
+        )
+    }
+    return (
+        <>
+            <div className="popup__row popup__row--title">
+                <div onClick={() => goTo('settings')} className="popup__btn popup__btn--back"></div>
+                <div className="popup__title">{!me?.email ? "Добавить почту" : "Изменить почту"}</div>
+                <div onClick={close} className="popup__btn popup__btn--close"></div>
+            </div>
+            <div className="popup__row popup__row--centered">
+                <div className="profile__input-simple-wrapper">
+                    <div className="profile__input-simple-row">
+                        <div className="profile__input-simple-title">Почта</div>
+                        <input className="profile__input-simple-input" type="text" name="" value={mail}
+                               onChange={(e) => setMail(e.target.value)} placeholder=""/>
+                    </div>
+                </div>
+            </div>
+            <div onClick={click} className="button button--red">Применить</div>
+        </>
+    )
 }
 
 export default AddMail
